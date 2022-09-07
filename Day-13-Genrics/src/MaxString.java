da01ddc3ea7961fea099042ae20f4bee94e5a43c
Anshul <you@example.com>
2022-09-07 19:19:37 +0530

    here we done programe using genrics

---
+++ Day-13-Genrics/src/MaxString.java
@@ -0,0 +1,21 @@
+
+public class MaxString {
+	
+	public static void main(String[] args) { 
+	    String[] FruitNames = {"appple", "banana", "peach"};
+	    String a= getLongestString(FruitNames);
+	    System.out.println(a);
+	}
+
+	public static String getLongestString(String []FruitNames) {
+		
+	    int j = 0;
+	    for (j = 0; j <= FruitNames.length; j++) {
+	        if (FruitNames[j].length() > FruitNames[j + 1].length()) {
+	                return (FruitNames[j]);
+	            }
+	        }
+	        return null;
+	    }
+
+}
